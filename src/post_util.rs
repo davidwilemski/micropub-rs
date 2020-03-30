75efba37fa0b5fdf76eaf16a4de6a65ca4cd47e8
David Wilemski <david@wilemski.org>
2020-03-29 20:58:35 -0700

    Add helper for generating URL slugs
    There will be cases where a slug is not provided as part of creating a
    new entry/post. When that is the case, we'll still want a slug to be
    created - this is a first pass on doing so.
    
    It first looks for a title/name of the post and uses that. If that does
    not exist then it will use the first bit of the content. It will URL
    encode the whole thing before returning.
    
    This is meant to be used only if a slug isn't provided and so doesn't
    take into account a slug property being sent as part of the micropub
    creation POST. That isn't currently a supported property for this server
    but will go on the TODO list.

---
+++ src/post_util.rs
@@ -0,0 +1,53 @@
+use url::form_urlencoded;
+
+pub fn get_slug(name: Option<&str>, content: &str) -> String {
+    let unencoded_slug = match name {
+        Some(n) => n,
+        // TODO might want to use a date/time instead for not slugs in the future?
+        // maybe %Y%m%d-%H%M%S
+        None => {
+            let end = content.len().min(32);
+            &content[0..end]
+        }
+    };
+
+    form_urlencoded::byte_serialize(
+        unencoded_slug.replace(" ", "-").as_bytes()
+    ).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::get_slug;
+
+    #[test]
+    fn it_uses_name_if_name_exists() {
+        assert_eq!(
+            get_slug(Some("testing"), "nothing"),
+            "testing"
+        );
+    }
+
+    #[test]
+    fn it_replaces_spaces_in_name_with_hyphens() {
+        assert_eq!(
+            get_slug(Some("testing stuff"), "nothing"),
+            "testing-stuff"
+        );
+    }
+
+    #[test]
+    fn it_uses_content_if_no_name() {
+        assert_eq!(
+            get_slug(None, "nothing"),
+            "nothing"
+        );
+    }
+    #[test]
+    fn it_truncates_content_for_slug() {
+        assert_eq!(
+            get_slug(None, "nothing: this is a rather long title"),
+            "nothing%3A-this-is-a-rather-long-t"
+        );
+    }
+}
