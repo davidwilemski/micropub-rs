d0840a744e157b300dd6931c5d296abff4c6f569
David Wilemski <david@wilemski.org>
2022-12-28 22:26:18 -0600

    Make server listen on all v4 addresses
    Apparently listening on localhost isn't sufficient for my current
    container port publishing configuration. I want to come back to this but
    this is what I need for now (and matches the warp implementation).

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -190,7 +190,7 @@ async fn main() -> Result<(), anyhow::Error> {
             )
         );
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3030));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
